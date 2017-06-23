b044a818fa3d37007752f12e4db27c52162cf8b5
Lom-Ali <programmerbox1993@mail.ru>
2017-06-23 05:36:55 +0300

    First commit before starting doing smth

---
+++ DatabaseFun/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+    year_formed = models.PositiveIntegerField()
+
+class Album(models.Model):
+    name = models.CharField(max_length=50)
---
+++ mvavenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/leon/Desktop/DjangoMVA/mvavenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
