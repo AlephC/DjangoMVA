73f54ca36fcb775cd2b46953ea27388c59828230
Lom-Ali <programmerbox1993@mail.ru>
2017-06-23 06:12:42 +0300

    Second lesson Done (Building a database)

--- DatabaseFun/app/models.py
+++ DatabaseFun/app/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Artist(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField("artist", max_length=50)
     year_formed = models.PositiveIntegerField()
 
 class Album(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField("album", max_length=50)
+    artist = models.ForeignKey(Artist)
